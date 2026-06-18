ce32c08eb16481a3e9575c4365ffdd22d22b6e7b
Masfiq Rahman Misha <masfiqrahmanmisha@gmail.com>
2026-06-19 01:55:45 +0600

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ let msgContainer = document.querySelector(".msg-container");
 let msg = document.querySelector("#msg");
 
 let turnO = true;
-let gameOver = false; // New flag
+let gameOver = false;
 
 const winPatterns = [
     [0, 1, 2],
@@ -20,15 +20,18 @@ const winPatterns = [
 
 const resetGame = () => {
     turnO = true;
-    gameOver = false; // reset flag
+    gameOver = false;
+
     enableBoxes();
+
     msgContainer.classList.add("hide");
+    newGameBtn.classList.add("hide");
 };
 
 boxes.forEach((box) => {
     box.addEventListener("click", () => {
-        if (gameOver) return; // prevent click after game over
-        if (box.innerText !== "") return; // prevent overwrite
+
+        if (gameOver || box.innerText !== "") return;
 
         if (turnO) {
             box.innerText = "O";
@@ -37,6 +40,7 @@ boxes.forEach((box) => {
             box.innerText = "X";
             turnO = true;
         }
+
         box.disabled = true;
 
         checkWinner();
@@ -44,37 +48,76 @@ boxes.forEach((box) => {
 });
 
 const disableBoxes = () => {
-    boxes.forEach((box) => (box.disabled = true));
+    boxes.forEach((box) => {
+        box.disabled = true;
+    });
 };
 
 const enableBoxes = () => {
     boxes.forEach((box) => {
         box.disabled = false;
         box.innerText = "";
+        box.classList.remove("winner");
     });
 };
 
-const showWinner = (winner) => {
-    msg.innerText = `🎉 Congratulations! Winner is ${winner}`;
+const showWinner = (winner, pattern) => {
+
+    pattern.forEach((index) => {
+        boxes[index].classList.add("winner");
+    });
+
+    msg.innerText = `Winner: ${winner}`;
     msgContainer.classList.remove("hide");
-    gameOver = true; // set flag
-    disableBoxes(); // disable all boxes
+
+    newGameBtn.classList.remove("hide");
+
+    gameOver = true;
+    disableBoxes();
+};
+
+const showDraw = () => {
+
+    msg.innerText = "Match Draw!";
+    msgContainer.classList.remove("hide");
+
+    newGameBtn.classList.remove("hide");
+
+    gameOver = true;
+    disableBoxes();
 };
 
 const checkWinner = () => {
+
     for (let pattern of winPatterns) {
+
         let [a, b, c] = pattern;
+
         let val1 = boxes[a].innerText;
         let val2 = boxes[b].innerText;
         let val3 = boxes[c].innerText;
 
         if (val1 !== "" && val1 === val2 && val2 === val3) {
-            showWinner(val1);
+            showWinner(val1, pattern);
             return;
         }
     }
+
+    let filledBoxes = 0;
+
+    boxes.forEach((box) => {
+        if (box.innerText !== "") {
+            filledBoxes++;
+        }
+    });
+
+    if (filledBoxes === 9) {
+        showDraw();
+    }
 };
 
-// Event listeners
 newGameBtn.addEventListener("click", resetGame);
 resetBtn.addEventListener("click", resetGame);
+
+// Hide New Game button when page loads
+newGameBtn.classList.add("hide");
